b21f300d35ccedd881346655cecf2fb1fcb35f2c
Hara Chung <harachung90@gmail.com>
2023-02-13 20:05:07 +1100

    Second commit. Created config.ini; Made html file for the user to enter their input

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-from flask import Flask
+import configparser
 import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def get_weather_results(zip_code, api_key):
-    api_url = "https://api.openweathermap.org/data/2.5/weather?zip={},AU&appid={}".format(zip_code, api_key)
-    print(api_url)
 
-get_weather_results("3144", "885d7114d1101e5a04be8067a4d3a8c5")
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def weather_dashboard():
+    return render_template('home.html')
+
+
+@app.route('/results')
+def render_results():
+    return "Results Page"
 
 
 if __name__ == '__main__':
     app.run()
+
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
+
+
+def get_weather_results(zip_code, api_key):
+    api_url = "https://api.openweathermap.org/data/2.5/weather?zip={},AU&appid={}".format(zip_code, api_key)
+    r = requests.get(api_url)
+    return r.json()
+
+
+print(get_weather_results("3144", get_api_key()))
